e55e3733ab1f98b30877f47b1a1817c7a16288cc
borges <nicolasborges736@gmail.com>
2025-11-27 13:51:06 -0300

    versão 1.0

---
+++ src/main/java/br/dev/borgesAirports/AirportsApplication.java
@@ -0,0 +1,13 @@
+package br.dev.borgesAirports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportsApplication.class, args);
+	}
+
+}
